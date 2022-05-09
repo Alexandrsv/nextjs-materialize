b476e9b59e511e4d60135e1a4d93821b6bb3cbe5
Александр <zbc@mail.ru>
2022-05-09 23:16:18 +0300

    add gh-deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+console.log({ debug });
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? "/nextjs-materialize/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
